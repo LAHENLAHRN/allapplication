c161681bc1c598ef674918f877f8dcba57b26b50
LAHENLAHRN <lahenlahrn@gmail.com>
2025-11-27 17:52:00 +0300

    bottom navigatoin a

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,17 @@
 import 'package:flutter/material.dart';
+import 'bottom_nav.dart';
+
 void main() {
-  runApp(HomePage());
-}
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
+  runApp(MyApp());
 }
 
-class _HomePageState extends State<HomePage> {
-  TextEditingController text1 = TextEditingController();
-  TextEditingController text2 = TextEditingController();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue.shade50,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            TextField(
-              controller: text1,
-              decoration: InputDecoration(
-                labelText: "أدخل النص",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            SizedBox(height: 20),
-
-            TextField(
-              controller: text2,
-              readOnly: true,
-              decoration: InputDecoration(
-                labelText: "هنا يظهر النص",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            SizedBox(height: 20),
-
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  text2.text = text1.text;
-                });
-              },
-              child: Text("طباعة النص"),
-            ),
-          ],
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomNavController(),
     );
   }
 }
+
